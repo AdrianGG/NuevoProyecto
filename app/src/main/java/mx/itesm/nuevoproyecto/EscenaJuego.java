c67cdbc9e7818496e58ae4999ace955112b58f4d
IramMolina <a01374040@itesm.mx>
2015-10-23 08:23:40 +0200

    Historia
    Hace cambio de imagen cuando detecta un touch, funciona con una lista de 3 escenas pero se pueden agregar mas :)

--- app/src/main/java/mx/itesm/nuevoproyecto/EscenaJuego.java
+++ app/src/main/java/mx/itesm/nuevoproyecto/EscenaJuego.java
@@ -1,5 +1,4 @@
 package mx.itesm.nuevoproyecto;
-
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
@@ -8,23 +7,55 @@ import org.andengine.opengl.texture.region.ITextureRegion;
  * Created by A. iram on 01/10/2015.
  */
 public class EscenaJuego extends EscenaBase {
-    private ITextureRegion regionFondo;
-    // private ITextureRegion regionBtnJugar; //boton de comenzar
+   
+   private ITextureRegion[] regionSlides;
+    private ITextureRegion regionSlideActual;
     private Sprite spriteFondo;
 
 
 
+
+    
     @Override
     public void cargarRecursos() {
-        regionFondo = cargarImagen("escenasiguiente.jpg");
+      //  regionFondo = cargarImagen("escenasiguiente.jpg");
+        regionSlides = new ITextureRegion[16];
+        regionSlides[0]=cargarImagen("creditos.jpg");
+        regionSlides[1] = cargarImagen("escenasiguiente.jpg");
+        regionSlides[2] = cargarImagen("fondomenu.jpg");
+        regionSlideActual=regionSlides[0];
+
 
     }
 
     @Override
     public void crearEscena() {
-        spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionFondo);
+      //  spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionFondo);
+       // attachChild(spriteFondo);
+        spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionSlideActual);
         attachChild(spriteFondo);
+        setOnSceneTouchListener(this.getOnSceneTouchListener());
 
+
+    }
+    public boolean onSceneTouchEvent(TouchEvent pSceneTouchEvent)
+    {
+        if (pSceneTouchEvent.isActionDown())
+        {
+            if(contador<2){
+                contador++;
+                regionSlideActual=regionSlides[contador];
+                spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionSlideActual);
+                attachChild(spriteFondo);
+            }
+            else{
+                admEscenas.crearEscenaJuego1();
+                admEscenas.setEscena(TipoEscena.ESCENA_JUEGO1);
+                admEscenas.liberarEscenaJuego();
+            }
+
+        }
+        return false;
     }
 
 
@@ -42,25 +73,18 @@ public class EscenaJuego extends EscenaBase {
 
     @Override
     public void liberarEscena() {
+        liberarRecursos();
         this.detachSelf();
         this.dispose();
     }
 
     @Override
     public void liberarRecursos() {
-        regionFondo.getTexture().unload();
-        regionFondo = null;
+        regionSlideActual.getTexture().unload();
+        regionSlideActual = null;
+        regionSlides=null;
     }
-
-    public boolean onSceneTouchEvent(TouchEvent pSceneTouchEvent) {
-        if (pSceneTouchEvent.isActionDown()) {
-            //	 instrucciones en pantala, toca para continuar,El	usuario	toca	la	pantalla y va a primer nivel
-            admEscenas.crearEscenaJuego1();
-            admEscenas.setEscena(TipoEscena.ESCENA_JUEGO1);
-            admEscenas.liberarEscenaJuego();
+   int  contador=0;
 
 
-        }
-        return super.onSceneTouchEvent(pSceneTouchEvent);
-    }
 }
